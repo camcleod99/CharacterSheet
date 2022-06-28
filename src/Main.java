36baecccfee5c9589dbdf4756aa42c669815f4ec
Craig McLeod <mail@camcleod.com>
2022-06-28 10:30:59 +0100

    220628@475_0.02 - Loop created for character creation

--- src/Main.java
+++ src/Main.java
@@ -9,12 +9,14 @@ import java.util.Scanner;
  * not be used in lieu of software that is used for D&D)
  */
 public class Main {
+    //Globals
+    public static final String[] statNames = {"STR","DEX","CON","INT","WIS","CHA"};
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         menu();
     }
 
     public static void menu(){
-        Scanner sc = new Scanner(System.in);
         boolean quit = false;
         printMenu();
         while(!quit){
@@ -31,7 +33,7 @@ public class Main {
                     quit = true;
                     break;
                 case 1:
-                    System.out.println("# Let's Make a Character!");
+                    System.out.println("Let's Make a Character!");
                     makeCharacter();
                     quit = true;
                     break;
@@ -49,41 +51,71 @@ public class Main {
         System.out.println("X - Anything Else");
     }
 
+/**
+ * Makes a character who has a block of stats, three attacks, three passives and
+ * starting equipment based on class.
+ */
+    //TODO: ATTACKS
+    //TODO: PASSIVES
+    //TODO: EQUIPMENT
     public static void makeCharacter(){
-        System.out.println("For the sake of speed, we're gonna make a human fighter today");
-        System.out.println("Let's Roll for your scores!");
-        // scores is where the six overall scores will be kept
-        // tempScores is used for keeping the dice rolls as they're made 
-        // tempScoreSum finds the sum of the three highest rolls (Done by sorting the array and then adding index's 1 2 and 3)
         int[]scores = new int[6];
         int[]tempScores = new int[4];
         int tempScoresSum;
+        boolean characterMade = false;
 
-        // For this we are using the "4 Drop lowest" rule
-        // As the title suggests, 4d6 are rolled and the lowest dice is dropped, giving your score out of 18
-        for(int i = 0; i < 6; i++){
-            for(int j = 0; j < 4; j++){
-                tempScores[j] = (int)(6.0 * Math.random()) + 1;
+        System.out.println("Let's Roll for your scores!");
+
+        while(!characterMade) {
+            // TODO: Put Generation here
+            // For stat generation we are using the "4 Drop lowest" rule
+            // As the title suggests, 4d6 are rolled and the lowest dice is dropped, giving your score out of 18
+            for (int i = 0; i < 6; i++) {
+                for (int j = 0; j < 4; j++) {
+                    tempScores[j] = (int) (6.0 * Math.random()) + 1;
+                }
+                Arrays.sort(tempScores);
+                System.out.println("Your rolls for "+ statNames[i] +" are:");
+                for (int k = 0; k < tempScores.length; k++) {
+                    System.out.print(tempScores[k] + " ");
+                }
+                System.out.println();
+                System.out.println("We drop the lowest score, that's " + tempScores[0]);
+                tempScoresSum = tempScores[1] + tempScores[2] + tempScores[3];
+                System.out.println("Your score for "+ statNames[i] +" is..." + tempScoresSum);
+                scores[i] = tempScoresSum;
+                System.out.println();
             }
-            Arrays.sort(tempScores);
-            System.out.println("Your rolls are:");
-            for(int k = 0; k < tempScores.length; k++){
-                System.out.print(tempScores[k]+" ");
+
+            System.out.println("Your Scores are;");
+            for (int i = 0; i < scores.length; i++) {
+                System.out.println(statNames[i] + " : " + scores[i]);
             }
             System.out.println();
-            System.out.println("We drop the lowest score, that's "+tempScores[0]);
-            tempScoresSum = tempScores[1]+tempScores[2]+tempScores[3];
-            System.out.println("Score #"+(i + 1)+" is..."+tempScoresSum);
-            scores[i] = tempScoresSum;
-            System.out.println();
-        }
 
-        Arrays.sort(scores);
-        System.out.println("Your Scores are:");
-        for(int i = 0; i < scores.length; i++){
-            System.out.print(scores[i]+" ");
+            System.out.println("Are you OK with these stats?");
+            String answer = sc.next();
+
+            switch(answer) {
+                case "y":
+                case "Y":
+                case "yes":
+                case "Yes":
+                    characterMade = true;
+                    System.out.println("Great! Your character is made!");
+                    break;
+                case "n":
+                case "N":
+                case "no":
+                case "No":
+                    characterMade = false;
+                    System.out.println("OK, let's roll again!");
+                    break;
+                default:
+                    characterMade = false;
+                    System.out.println("what are you even saying?");
+                    break;
+            }
         }
-        System.out.println();
     }
-
 }
